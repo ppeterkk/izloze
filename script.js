426d3aa988fcabd57d8996a9901e34834276c021
ppeterkk <peteris.kipsts@gmail.com>
2022-03-08 13:16:21 +0200

    Pirmais

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ const naudaKopa = 10000000;
 
 let uzvaretajuSkaits = 5;
 
+let balva = 5;
+
 let rindas = document.querySelector('.rindas'); //pievieno mainīgo
+
+rindas.innerHTML = ' ';
 
 for (let i=0;i<uzvaretajuSkaits; i++){
     let rand = Math.random()*vardi.length;
@@ -14,10 +18,15 @@ for (let i=0;i<uzvaretajuSkaits; i++){
     //noapaļo uz leju
     let uzvaretajuSkaits = vardi[rand];
     let uzvaretajs = (vardi[rand]);
+    let balva = balvas[rand];
     //izvada konsolē
 rindas.innerHTML +=`
-//pievieno +, lai pieskaitītu nākamo
 <tr>
     <td>${i+1}</td>
     <td>${uzvaretajs}</td>
+    <td>${balva}</td>
 </tr>` //izvade vairākās rindās
+}
+
+
+//rindas.innerHTML - pievieno +, lai pieskaitītu nākamo
