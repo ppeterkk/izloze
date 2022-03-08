f71e01329db7475cc051c740055c5d6b7738cfc9
ppeterkk <peteris.kipsts@gmail.com>
2022-03-08 10:18:27 +0200

    Pirmais

---
+++ script.js
@@ -0,0 +1,9 @@
+const vardi = ['Baiba Krūmiņa', 'Jānis Bērziņš', 'Pēteris Liepa', 'Laura Ose', 'Artis Priede'];
+
+const balvas = ['Dators', 'Telefons', 'Blenderis', 'Fotoaparāts', 'Kafijas automāts'];
+
+const naudaKopa = 10000000;
+
+let uzvaretajuSkaits = 5;
+
+console.log (vardi[rand]);
